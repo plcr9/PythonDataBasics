f5b9c2858a829de2aeb4fcb34c56c3e6f261c405
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-11 08:07:08 +0100

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
 
+speed = [99,86,87,88,86,103,87,94,78,77,85,86]
+
+x = np.mean(speed)
+y = np.median(speed)
+z = np.mode(speed)
+
+print(x)
+print(y)
+print(z)
