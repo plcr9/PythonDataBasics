72456267dd3e08b517a59266f12035aba92fdf8d
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-16 08:27:13 +0100

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,1 +1,11 @@
-
+import pandas as pd
+x = pd.Series(range(10, 20))
+y = pd.Series([2, 1, 4, 5, 8, 12, 18, 25, 96, 48])
+x.corr(y)                     # Pearson's r
+0.7586402890911867
+y.corr(x)
+0.7586402890911869
+x.corr(y, method='spearman')  # Spearman's rho
+0.9757575757575757
+x.corr(y, method='kendall')   # Kendall's tau
+0.911111111111111
