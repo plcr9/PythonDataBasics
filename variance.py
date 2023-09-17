a51aa2203abdedfd7bd5ecf25e22595f99aa49c3
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-17 06:29:20 +0100

    Update variance.py

--- variance.py
+++ variance.py
@@ -1,6 +1,6 @@
 import statistics
 
-print(statistics.variance([1, 3, 5, 7, 9, 11]))
-print(statistics.variance([2, 2.5, 1.25, 3.1, 1.75, 2.8]))
+print(statistics.variance([1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31]))
+print(statistics.variance([2, 2.5, 1.25, 3.1, 1.75, 2.8, 1.4, 2.85, 1.35, 2.7]))
 print(statistics.variance([-11, 5.5, -3.4, 7.1]))
 print(statistics.variance([1, 30, 50, 100]))
