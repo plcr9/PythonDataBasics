5ca6e49b623001f480f75552597769db2a4d6a24
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-18 16:20:45 +0100

    Update chi_square_test.py

--- chi_square_test.py
+++ chi_square_test.py
@@ -1,1 +1,11 @@
+from scipy.stats import chi2_contigency
 
+data = [[207, 282, 241], [234, 242, 232]]
+stat, p, dof, expected = chi2_contigency(data)
+
+alpha = 0.05
+print("p value is " + str(p))
+if p <= alpha:
+  print('Dependent (reject H0)')
+else:
+  print('Independent (H0 holds true)')
