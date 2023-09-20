4b9bd9ea951b7a7ed23bba00f62e67eec268a4e9
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-21 00:21:20 +0100

    Update mean.py

--- mean.py
+++ mean.py
@@ -1,1 +1,7 @@
+import statistics
 
+data = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400]
+
+x = statistics.mean(data)
+
+print("The mean of this dataset is:", x)
