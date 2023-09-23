d695b94d0ebf2bba449d35a817b200594249bc15
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-23 11:24:34 +0100

    Update median.py

--- median.py
+++ median.py
@@ -1,1 +1,10 @@
+import statistics
 
+data = [1, 8, 27, 64, 125, 256, 343, 512, 729]
+data2 = [1, 8, 27, 64, 125, 256, 343, 512, 729, 1000]
+
+x = statistics.median(data)
+y = statistics.median(data2)
+
+print("The median of this dataset is:", x)
+print("The median of this dataset is:", y)
