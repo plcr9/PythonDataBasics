a4f64bacf773bc126b0c9d71e85e94b2a39f9612
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-26 10:13:38 +0100

    Update first_non_consecutive_number.py

--- first_non_consecutive_number.py
+++ first_non_consecutive_number.py
@@ -1,1 +1,10 @@
+def first_non_consecutive(arr):
+  if not arr: return 0
+  for i, x in enumerate(arr[:-1]:
+    if x + 1 != arr[i+1]:
+      return arr[i+1]
 
+def basic_test_cases():
+  test.assert_equals(first_non_consecutive([1,2,3,4,6,7,8], 6)
+  test.assert_equals(first_non_consecutive([1,2,3,4,5,6,7,8], None)
+  test.assert_equals(first_non_consecutive([-3,-2,0,1],0)
