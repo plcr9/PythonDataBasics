62d59193ae2f4f4298c3adda86d8f8c75bc255a9
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-27 10:56:38 +0100

    Update odd_number_out.py

--- odd_number_out.py
+++ odd_number_out.py
@@ -1,1 +1,12 @@
+from collections import Counter
 
+def stray(arr):
+  result = 0
+  for num in arr:
+    result ^= num
+  return result
+
+def basic_test_cases():
+  test.assert_equals(stray([1,1,1,1,1,1,2],2)
+  test.assert_equals(stray([2,3,2,2,2],3)
+  test.assert_equals(stray([3,2,2,2,2],3)
