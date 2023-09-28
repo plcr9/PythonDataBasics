5ff08a3233fffed5b0378f219e6955dc41aebe68
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-28 10:27:29 +0100

    Update decimal_digits_num.py

--- decimal_digits_num.py
+++ decimal_digits_num.py
@@ -1,1 +1,7 @@
+def digits(number):
+  return len(str(number))
 
+def example_tests():
+  test.assert_equals(digits(5),1)
+  test.assert_equals(digits(12345),5)
+  test.assert_equals(digits(9876543210),10)
