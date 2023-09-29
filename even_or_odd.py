226377e71ed9242eca59295d32277b29dd29a40a
Paul <118978940+plcr9@users.noreply.github.com>
2023-09-29 14:46:46 +0100

    Update even_or_odd.py

--- even_or_odd.py
+++ even_or_odd.py
@@ -1,1 +1,9 @@
+def even_or_odd(number):
+  return 'Odd' if number % 2 else 'Even'
 
+def basic_test_cases():
+  test.assert_equals(even_or_odd(2), 'Even')
+  test.assert_equals(even_or_odd(1), 'Odd')
+  test.assert_equals(even_or_odd(0), 'Even')
+  test.assert_equals(even_or_odd(15456452, 'Even')
+  test.assert_equals(even_or_odd(-123), 'Odd')
