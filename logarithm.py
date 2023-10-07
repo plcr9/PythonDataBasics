5740d31375267d55e0ebe28e811d6c7e22e7093d
Paul <118978940+plcr9@users.noreply.github.com>
2023-10-07 12:51:35 +0100

    Create logarithm.py

---
+++ logarithm.py
@@ -0,0 +1,15 @@
+import math
+
+x = 30
+
+# Returns natural logarithm of a number or the logarithm of number to base
+print(math.log(x))
+
+# Returns the base-10 logarithm of x
+print(math.log10(x))
+
+# Returns the natural logarithm of 1+x
+print(math.log1p(x))
+
+# Returns the base-2 logarithm of x
+print(math.log2(x))
